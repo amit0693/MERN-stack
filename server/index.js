5263ec428bbb81812043566ee40a354cfa4d173c
Amit Pandit <90578817+amit0693@users.noreply.github.com>
2022-03-31 16:49:09 -0400

    Mongo Start

--- server/index.js
+++ server/index.js
@@ -3,4 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb://amitpandit:Amit0693@cluster0-shard-00-00.e0aut.mongodb.net:27017,cluster0-shard-00-01.e0aut.mongodb.net:27017,cluster0-shard-00-02.e0aut.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-von5gz-shard-0&authSource=admin&retryWrites=true&w=majority';
+const PORT= process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port :${PORT}`)))
+    .catch((error) => console.log(error.message) );
+
+

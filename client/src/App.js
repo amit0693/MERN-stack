01b2d98d363d08ae4edfdec7a63e23cc478a0ba3
Amit Pandit <90578817+amit0693@users.noreply.github.com>
2022-04-01 19:35:44 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,13 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
       <Grow in>
         <Container>
-          <Grid className={classes.mainContainer}container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>

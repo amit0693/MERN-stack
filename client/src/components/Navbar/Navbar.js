90ec7d0fba00a80fd80eefc7a9f211885747ee45
Amit Pandit <90578817+amit0693@users.noreply.github.com>
2022-04-01 14:39:53 -0400

    4#

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import { AppBar , Typography} from "@material-ui/core";
+import React from "react";
+import useStyles from './styles';
+
+
+const Navbar = () => {
+    const classes = useStyles()
+
+    return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+)
+}
